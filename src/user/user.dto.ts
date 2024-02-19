a31215729addb7502fcfcc92d2357c5db2bc474d
4eJIoBeG <pavelshashenko@gmail.com>
2024-02-19 20:11:17 +0300

    finish task module

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -31,14 +31,14 @@ export class UserDto extends PomodoroSettingsDto {
 	@IsOptional()
 	email?: string
 
-	@IsOptional()
-	@MinLength(6, {
-		message: 'Password must be at least 6 characters long'
-	})
 	@IsString()
 	@IsOptional()
 	name?: string
 
+	@IsOptional()
+	@MinLength(6, {
+		message: 'Password must be at least 6 characters long'
+	})
 	@IsString()
 	@IsOptional()
 	password?: string
